ef8e138c456cb6d48fc1f2fd9737a230469b15ce
Mohamed Amine <hiddentn@outlook.com>
2023-12-14 13:12:49 +0100

    restructured project

---
+++ c-sharp/src/advent-of-code/2023/Day15/Day15.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode._2023.Day15;
+
+public class Day15Solver : DaySolver
+{
+	public Day15Solver(DaySolverOptions options) : base(options)
+	{
+	}
+
+	public override string Day => "15";
+	public override string Year => "2023";
+
+
+	public override string SolvePart1()
+	{
+		return "";
+	}
+
+
+	public override string SolvePart2()
+	{
+		return "";
+	}
+}
